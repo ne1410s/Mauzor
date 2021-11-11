f815d7f94d4351bb81844386d434fa1591e8495e
ne1410s <pgj646@gmail.com>
2021-11-11 19:17:57 +0000

    Add project files.

---
+++ Mauzor.UI/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace Mauzor.UI
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
